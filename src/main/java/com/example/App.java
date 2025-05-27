8600e499e9ffbd6b82ed7869697ee4c67022a19d
souzabg <bruno.garbuio.souza@gmail.com>
2025-05-27 14:50:09 -0500

    minor changes to the basic program

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,28 +3,52 @@ package com.example;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
 
 public class App extends Application {
 
-    private Scene scene;
 
     @Override
     public void start(Stage stage) {
-        Button btn = new Button("Say Hello");
-        btn.setOnAction(e -> System.out.println("Hello from JavaFX!"));
+        // Create a button
+        Button btn = new Button("Click me!");
+        Button btn2 = new Button("Label Button!");
+        
+        Label label = new Label("You have not pushed the button!");
 
-        VBox layout = new VBox(10, btn);
+
+        // Button click event
+        btn.setOnAction(e -> System.out.println("Test button"));
+        btn2.setOnAction (e -> label.setText("You have pushed the button!"));
+
+        //Adding an image
+        Image image = new Image(getClass().getResource("/bruno-morningwood.jpg").toExternalForm());
+        ImageView imageview = new ImageView(image);
+
+        //Modifying an image
+        imageview.setFitWidth(300);
+        imageview.setPreserveRatio(true);
+
+        // Create a layout and add the button
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(imageview, btn, btn2, label);
         layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
 
-        Scene scene = new Scene(layout, 400, 300);
+        // Create a scene with the layout
+        Scene scene = new Scene(layout, 1200, 800);
+
+        // Setup the stage (window)
+        stage.setTitle("Day 1 - Hello JavaFX");
         stage.setScene(scene);
-        stage.setTitle("My First JavaFX App");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch();  // Start the app
     }
 }
